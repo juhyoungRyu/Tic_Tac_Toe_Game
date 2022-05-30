41cf92b42e92f1589078542c48e20d97455061de
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-31 00:45:37 +0900

    로직 구상 중

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Game.css";
 import { Grid } from "./Grid";
 
 export const Game = () => {
+  const [girdBoolean, setGridBoolean] = useState(
+    { one: null, four: null, seven: null }, // Grid1
+    { two: null, five: null, eight: null }, // Grid2
+    { three: null, six: null, nine: null } // Grid3
+  );
+
+  const [userArr, setUserArr] = useState([]);
+  const [comArr, setComArr] = useState([]);
+
+  const clickBox = (position) => {
+    let arrTemp = [...userArr];
+    arrTemp.push(position);
+    setUserArr(arrTemp);
+  };
+
+  useEffect(() => {
+    return console.log(userArr);
+  }, [userArr]);
+
   return (
     <div className="Game">
-      <Grid />
+      <section className="grid1">
+        <Grid click={clickBox} position={1} status={girdBoolean.one} />
+        <Grid click={clickBox} position={4} status={girdBoolean.two} />
+        <Grid click={clickBox} position={7} status={girdBoolean.three} />
+      </section>
+      <section className="grid2">
+        <Grid click={clickBox} position={2} status={girdBoolean.four} />
+        <Grid click={clickBox} position={5} status={girdBoolean.five} />
+        <Grid click={clickBox} position={8} status={girdBoolean.six} />
+      </section>
+      <section className="grid3">
+        <Grid click={clickBox} position={3} status={girdBoolean.seven} />
+        <Grid click={clickBox} position={6} status={girdBoolean.eight} />
+        <Grid click={clickBox} position={9} status={girdBoolean.nine} />
+      </section>
     </div>
   );
 };
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Gird.css";
 
-export const Grid = () => {
-  return <div className="Grid">Grid</div>;
+export const Grid = (props) => {
+  const [girdStatus, setGridStatus] = useState(false);
+
+  useEffect(() => {
+    if (props.status != null) {
+      setGridStatus(true);
+    }
+  }, [props.status]);
+
+  return (
+    <div className="Grid" onClick={() => props.click(props.position)}></div>
+  );
 };
